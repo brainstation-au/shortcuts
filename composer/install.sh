23c87e9c005dcaae63425edda2a506d57cd39e7d
Khalid Saifullah <khalid@outlook.com.au>
2017-04-29 13:39:55 +1000

    Add code reference

--- composer/install.sh
+++ composer/install.sh
@@ -9,6 +9,8 @@ else
   return
 fi
 
+# https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
+
 EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
