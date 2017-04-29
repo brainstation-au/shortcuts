bf1c3cd73018d9f9df06c14dad6688e1f7f96a26
Khalid Saifullah <khalid@outlook.com.au>
2017-04-29 12:28:32 +1000

    Check if composer is already installed or not.

--- composer/install.sh
+++ composer/install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Check if already installed.
+which composer &> /dev/null
+if [[ $? == 1 ]]; then :
+  # If exit status is 1, go and install below.
+else
+  echo "Docker already exists."
+  return
+fi
+
 EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
