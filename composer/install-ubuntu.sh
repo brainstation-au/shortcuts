e118c77001c1ad3ce571c9ecd496ac57f5c4f073
Khalid Saifullah <khalid@outlook.com.au>
2017-04-29 14:29:30 +1000

    Clean up.

--- composer/install-ubuntu.sh
+++ composer/install-ubuntu.sh
@@ -3,12 +3,12 @@
 # Stop the execution of a script if a command or pipeline has an error.
 set -e
 
-# Print all executed commands to the terminal
+# Print all executed commands to the terminal.
 set -x
 
 # Check if already installed.
 which composer &> /dev/null
-if [[ $? == 1 ]]; then :
+if [ $? == 1 ]; then
   # If exit status is 1, go and install below.
 else
   echo "Composer already exists."
