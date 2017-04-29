6956720ed81c4463ee396d4da38a2caaafd2eb51
Khalid Saifullah <khalid@outlook.com.au>
2017-04-30 09:50:53 +1000

    return: can only `return' from a function or sourced script

--- docker/install-ubuntu.sh
+++ docker/install-ubuntu.sh
@@ -8,40 +8,39 @@ which docker &> /dev/null
 if [ $? == 0 ]
 then
   echo "Docker already exists."
-  return
+else
+  # Stop the execution of a script if a command or pipeline has an error.
+  set -e
+
+  # Print all executed commands to the terminal
+  set -x
+
+  # @see https://docs.docker.com/engine/installation/linux/ubuntu.
+
+  # Update package information.
+  apt-get update > /dev/null
+
+  # Install packages to allow apt to use a repository over HTTPS:
+  apt-get install apt-transport-https --assume-yes
+  apt-get install ca-certificates --assume-yes
+  apt-get install curl --assume-yes
+  apt-get install software-properties-common --assume-yes
+
+  # Add Docker’s official GPG key:
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+  # Set up the stable docker repository.
+  add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+  # Update package information.
+  apt-get update > /dev/null
+
+  # Install the latest version of Docker
+  apt-get install docker-ce --assume-yes
 fi
-
-# Stop the execution of a script if a command or pipeline has an error.
-set -e
-
-# Print all executed commands to the terminal
-set -x
-
-# @see https://docs.docker.com/engine/installation/linux/ubuntu.
-
-# Update package information.
-apt-get update > /dev/null
-
-# Install packages to allow apt to use a repository over HTTPS:
-apt-get install apt-transport-https --assume-yes
-apt-get install ca-certificates --assume-yes
-apt-get install curl --assume-yes
-apt-get install software-properties-common --assume-yes
-
-# Add Docker’s official GPG key:
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-# Set up the stable docker repository.
-add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
-
-# Update package information.
-apt-get update > /dev/null
-
-# Install the latest version of Docker
-apt-get install docker-ce --assume-yes
 
 # Add the docker group if it doesn't already exist.
 getent group docker || groupadd docker
