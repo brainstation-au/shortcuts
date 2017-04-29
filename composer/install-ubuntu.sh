7826129abc78e07d84bdedf15efa784ebcef85b5
Khalid Saifullah <khalid@outlook.com.au>
2017-04-29 14:40:51 +1000

    Clean up.

--- composer/install-ubuntu.sh
+++ composer/install-ubuntu.sh
@@ -8,18 +8,16 @@ set -x
 
 # Check if already installed.
 which composer &> /dev/null
-if [ $? == 0 ]; then
+if [[ $? == 0 ]]; then
   echo "Composer already exists."
   return
 fi
 
 # Check if wget is already installed.
 which wget &> /dev/null
-if [ $? == 1 ]; then
+if [[ $? == 1 ]]; then
   # Update package information.
   apt-get update > /dev/null
   # Install wget.
   apt-get install wget
 fi
-
-. install.sh
