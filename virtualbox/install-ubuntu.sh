43931ee4b66c98135f087bda5b3e499449a341ea
brainstation <brainstation@outlook.com.au>
2017-05-04 21:26:38 +1000

    Run installation with sudo.

--- virtualbox/install-ubuntu.sh
+++ virtualbox/install-ubuntu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Add virtualbox source to your repository.
-apt-add-repository "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib"
+sudo apt-add-repository "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib"
 
 # Check latest version number from https://www.virtualbox.org/wiki/Linux_Downloads.
 # Install your expected version of virtualbox.
-apt-get update
-apt-get install virtualbox-5.1
+sudo apt-get update
+sudo apt-get install virtualbox-5.1 -y
